18d8a91fa5c637c66e70813d92c2ce361342dd0d
Abronz Vizcarra <47616761+abronz@users.noreply.github.com>
2022-01-14 04:42:09 -0500

    environments bug fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  homeLink: 'http://localhost:4200/',
+  homeLink: 'https://spacestagram-oen0gm6dw-abronz.vercel.app/',
   nasaApodApi: 'https://api.nasa.gov/planetary/apod?api_key=QdSX6Muc9WIANLcmFjRHDsiuHWmh2U9JOOa2GnUY',
-  dbApi: 'http://localhost:8080/api'
+  dbApi: 'https://spacestagram-api.herokuapp.com/api'
 };
 
 /*
